7a314d1c432bf6cda13a77210fbc3a1fb9fc2501
Nicolas Floch <nfloch96@gmail.com>
2023-08-27 01:44:17 +0200

    feat: add FileReader service

---
+++ src/Service/FileReader.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Service;
+
+class FileReader
+{
+    /**
+     * @return iterable<string>
+     */
+    public function getIterator(string $filePath): iterable {
+        if ($file = fopen($filePath, "r")) {
+            while(!feof($file)) {
+                if (($line = fgets($file)) !== false) {
+                    yield $line;
+                }
+            }
+            fclose($file);
+        }
+    }
+}
---
+++ tests/Unit/Service/FileReaderTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Tests\Unit\Service;
+
+use App\Service\FileReader;
+use PHPUnit\Framework\TestCase;
+
+class FileReaderTest extends TestCase
+{
+
+    private FileReader $testedInstance;
+
+    protected function setUp(): void
+    {
+        $this->testedInstance = new FileReader();
+    }
+
+    public function testGetIterator(): void
+    {
+        $filePath = __DIR__ . "/../../data/test.txt";
+        $lines = [...$this->testedInstance->getIterator($filePath)];
+        $this->assertEquals("azerty\n", $lines[0]);
+        $this->assertEquals("123456\n", $lines[1]);
+        $this->assertCount(2, $lines);
+    }
+}
