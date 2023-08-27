a61624ed0c02635d2f2637dfe4facd13d769dbf8
Nicolas Floch <nfloch96@gmail.com>
2023-08-27 02:30:59 +0200

    feat: add GHArchiveEventTypeMapper and TemporaryFileWriter

---
+++ src/Service/GHArchiveEventTypeMapper.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\EventType;
+use RuntimeException;
+
+class GHArchiveEventTypeMapper
+{
+    public function isEventTypeAllowed(string $type): bool
+    {
+        $allowedTypes = [
+            "CommitCommentEvent",
+            "IssueCommentEvent",
+            "PullRequestReviewCommentEvent",
+            "PushEvent",
+            "PullRequestEvent"
+        ];
+
+        return in_array($type, $allowedTypes);
+    }
+
+    /**
+     * @return string The corresponding event type for entity
+     * @throws RuntimeException
+     */
+    public function transformEventType(string $ghArchiveType): string {
+
+        return match ($ghArchiveType) {
+            "CommitCommentEvent", "IssueCommentEvent", "PullRequestReviewCommentEvent" => EventType::COMMENT,
+            "PullRequestEvent" => EventType::PULL_REQUEST,
+            "PushEvent" => EventType::COMMIT,
+            default => throw new RuntimeException('Unknown ghArchive type')
+        };
+    }
+}
